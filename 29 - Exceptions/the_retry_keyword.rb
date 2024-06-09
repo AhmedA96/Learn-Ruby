34533b750982ec61df77aad94aa96c8a5b4546aa
Ahmed Ali <86827869+AhmedA96@users.noreply.github.com>
2024-06-09 08:17:31 +0300

    Update the_retry_keyword.rb

--- 29 - Exceptions/the_retry_keyword.rb
+++ 29 - Exceptions/the_retry_keyword.rb
@@ -1,16 +1,17 @@
 def sum(a, b)
-  # begin
-  a + b
-rescue TypeError => e # we will try to solve that error
-  a = a.to_i
-  b = b.to_i
-  # this will convert to integer type, but will not execite a + b again.
-  # so we use retry, to send that new logic to the begin section.
-  retry
-rescue NoMethodError => e
-  a = 0
-  b = 0
-  retry
+  begin
+    a + b
+  rescue TypeError => e # we will try to solve that error
+    a = a.to_i
+    b = b.to_i
+    # this will convert to integer type, but will not execite a + b again.
+    # so we use retry, to send that new logic to the begin section.
+    retry
+  rescue NoMethodError => e
+    a = 0
+    b = 0
+    retry
+  end
 end
 
 puts sum(1, 2)
