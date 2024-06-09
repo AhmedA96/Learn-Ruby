ca7608aa8f67dff68bb18d86470979fd79586f67
Ahmed Ali <86827869+AhmedA96@users.noreply.github.com>
2024-06-09 08:15:59 +0300

    Update the_begin_and_rescue_keywords.rb

--- 29 - Exceptions/the_begin_and_rescue_keywords.rb
+++ 29 - Exceptions/the_begin_and_rescue_keywords.rb
@@ -1,19 +1,18 @@
 # Exception - is an Error, a point in your code where Ruby can't correctly evalute.
-# May be syntactical mistake, or a logic mistake.
+# May be syntactical mistake, or a logic mistake, etc...
 
 def sum(a, b)
-  # begin keyword
+  begin 
   # it marks the beginning of a section or a block of code where we're going
   # to exceute something that might lead to an error
   a + b
-
-  # rescue keyword
+  rescue 
   # This is a section where we can rescue the error. This is where we can
   # step in to save the code. So what we can do is in this section of code,
   # is we write what to do in case an error occurs. And if you use the
   # rescue keyword just by itself, we're basically reacting to any kind of
   # error that can go wrong. rescue excuted what inside its body.
-  # 'Unknown'
+  'Unknown'
 end
 
 puts sum(3, 5)
