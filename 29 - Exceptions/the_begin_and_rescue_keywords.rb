c50f0ba128abc42b2b61e5c73bbb467287864f8f
Ahmed Ali <86827869+AhmedA96@users.noreply.github.com>
2024-06-09 08:19:27 +0300

    Update the_begin_and_rescue_keywords.rb

--- 29 - Exceptions/the_begin_and_rescue_keywords.rb
+++ 29 - Exceptions/the_begin_and_rescue_keywords.rb
@@ -5,14 +5,14 @@ def sum(a, b)
   begin 
   # it marks the beginning of a section or a block of code where we're going
   # to exceute something that might lead to an error
-  a + b
+    a + b
   rescue 
   # This is a section where we can rescue the error. This is where we can
   # step in to save the code. So what we can do is in this section of code,
   # is we write what to do in case an error occurs. And if you use the
   # rescue keyword just by itself, we're basically reacting to any kind of
   # error that can go wrong. rescue excuted what inside its body.
-  'Unknown'
+    'Unknown'
 end
 
 puts sum(3, 5)
