f1dd1286893e683ee4c8fd849d7b666b01c2bff6
Ahmed Ali <86827869+AhmedA96@users.noreply.github.com>
2024-06-09 08:18:58 +0300

    Update catching_error_object.rb

--- 29 - Exceptions/catching_error_object.rb
+++ 29 - Exceptions/catching_error_object.rb
@@ -1,13 +1,14 @@
 def sum(a, b)
-  # begin
-  a + b
-rescue TypeError => e # e is an error object, only rescuing TypeError
-  puts "CLASS NAME: #{e.class}"
-  puts "MESSAGE: #{e.message}" # message is an instance method for Error objects.
-rescue NoMethodError => e # Now rescuing NoMethodError
-  puts 'We will resolve NoMethodError'
-  puts "CLASS NAME: #{e.class}"
-  puts "MESSAGE: #{e.message}"
+  begin
+    a + b
+  rescue TypeError => e # e is an error object, only rescuing TypeError
+    puts "CLASS NAME: #{e.class}"
+    puts "MESSAGE: #{e.message}" # message is an instance method for Error objects.
+  rescue NoMethodError => e # Now rescuing NoMethodError
+    puts 'We will resolve NoMethodError'
+    puts "CLASS NAME: #{e.class}"
+    puts "MESSAGE: #{e.message}"
+  end
 end
 
 puts sum(1, 2)
